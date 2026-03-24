1e52dd663083817d78e2cc5db5f79cd01494c676
riponmondalbd <riponmondal149@gmail.com>
2026-03-25 00:50:10 +0600

    update user profile controller

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -29,3 +29,27 @@ export const getProfile = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+// update user profile name and email
+export const updateUserProfile = async (req: Request, res: Response) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const { name, email } = req.body;
+
+    const updatedUser = await prisma.user.update({
+      where: { id: req.user.id },
+      data: { name, email },
+      select: {
+        name: true,
+        email: true,
+      },
+    });
+
+    res.json({ message: "Profile updated successfully", user: updatedUser });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
