fe7abec21e6b5c56bc230097ceaf883ff44bea9b
riponmondalbd <riponmondal149@gmail.com>
2026-03-24 21:50:17 +0600

    add routes to main server

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import { prisma } from "../prisma/prisma";
 
 // register user
@@ -29,5 +30,35 @@ export const registerUser = async (req: Request, res: Response) => {
       .json({ message: "User registered successfully", userId: user.id });
   } catch (error) {
     res.status(500).json({ message: "Error registering user" });
+  }
+};
+
+// login user
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    // valid user check
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // generate JWT token
+    const token = jwt.sign(
+      { userId: user.id, role: user.role },
+      process.env.JWT_SECRET!,
+      { expiresIn: "1h" },
+    );
+
+    res.json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in user" });
   }
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import authRoutes from "./routes/auth.routes";
 
 // load environment variables
 dotenv.config();
@@ -17,8 +18,11 @@ app.get("/", (req, res) => {
   res.json({ message: "API is running" });
 });
 
+// add routes
+app.use("/api/auth", authRoutes);
+
 // start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
